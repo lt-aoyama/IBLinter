a8c0783eb8a9d8205144caaa6d9aaed67e518cee
aoyama <102977190+lt-aoyama@users.noreply.github.com>
2024-09-12 15:22:57 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.9
 
 import PackageDescription
 
 var package = Package(
     name: "IBLinter",
-    platforms: [.macOS(.v10_13)],
+    platforms: [.macOS(.v12)],
     products: [
         .executable(
             name: "iblinter", targets: ["IBLinter"]
@@ -21,10 +21,10 @@ var package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/IBDecodable/IBDecodable.git", from: "0.5.0"),
-        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.29.0"),
+        .package(url: "https://github.com/lt-aoyama/IBDecodable.git", from: "0.6.1"),
+        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.35.0"),
         .package(url: "https://github.com/phimage/XcodeProjKit.git", from: "2.2.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.1"),
     ],
     targets: [
         .target(
